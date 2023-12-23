06b80896a242e4b00197ba781e026240ad5044c6
Lenovo <chenqh8899@outlook.com>
2023-12-23 20:58:23 +0800

    SK与通义千问集成

---
+++ chat2knowl-server/Chat2KnowL.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using Chat2KnowL.Console;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var builder = Kernel.CreateBuilder();
+
+//通义千问
+builder.AddQwenChatCompletion(
+         "Qwen", "none", "http://20.127.159.255:8000/v1"
+         );
+
+var kernel = builder.Build();
+
+//等待用户输入
+Console.Write("用户：");
+var input = Console.ReadLine();
+
+//循环对话
+while (input != "quit")
+{
+    var prompt = @$"<message role=""user"">{input}</message>";
+    var summarize = kernel.CreateFunctionFromPrompt(prompt, executionSettings: new OpenAIPromptExecutionSettings { MaxTokens = 100, Temperature = 0.5 });
+    Console.Write("知识助理：");
+    var result =   kernel.InvokeStreamingAsync(summarize);
+    await foreach (var item in result)
+    {
+        Console.Write(item.ToString());
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.Write("用户：");
+    input = Console.ReadLine();
+}
---
+++ chat2knowl-server/Chat2KnowL.Console/QwenServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.TextGeneration;
+
+namespace Chat2KnowL.Console
+{
+    public static class QwenServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 添加通义千问聊天服务
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="modelId"></param>
+        /// <param name="apiKey"></param>
+        /// <param name="url"></param>
+        /// <param name="orgId"></param>
+        /// <param name="serviceId"></param>
+        /// <returns></returns>
+        public static IKernelBuilder AddQwenChatCompletion(this IKernelBuilder builder, string modelId, string apiKey, string url, string? orgId = null, string? serviceId = null)
+        {
+            string modelId2 = modelId;
+            string apiKey2 = apiKey;
+            string orgId2 = orgId;
+
+            Func<IServiceProvider, object, OpenAIChatCompletionService> implementationFactory = (IServiceProvider serviceProvider, object? _) => new OpenAIChatCompletionService(modelId, new OpenAIClient(new Uri(url), new Azure.AzureKeyCredential(apiKey)), serviceProvider.GetService<ILoggerFactory>());
+            builder.Services.AddKeyedSingleton((object?)serviceId, (Func<IServiceProvider, object?, IChatCompletionService>)implementationFactory);
+            builder.Services.AddKeyedSingleton((object?)serviceId, (Func<IServiceProvider, object?, ITextGenerationService>)implementationFactory);
+            return builder;
+        }
+    }
+}
