d7923bfe067c433e3d5a6407ea9d6a9a0c8116c1
Lenovo <chenqh8899@outlook.com>
2023-12-29 09:55:58 +0800

    vue.config.js

--- chat2knowl-client/vue.config.js
+++ chat2knowl-client/vue.config.js
@@ -1,4 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { defineConfig } = require('@vue/cli-service')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .use(new HtmlWebpackPlugin({
+        title:"Chat2KnowL - AI智能回答文档",
+        filename: "index.html",
+        templateParameters: {
+          BASE_URL: `/`
+        },
+        template: resolve("./public/index.html"),
+        inlineSource: ".(js|css)$",
+        inject: "body",
+      }))
+  }
 })
